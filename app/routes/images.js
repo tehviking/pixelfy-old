00b6f14e3358290b3d5cb054bff468ee5812cd40
Brandon Hays <brandon.hays@gmail.com>
2015-02-19 16:13:12 -0600

    add slider component

--- app/routes/images.js
+++ app/routes/images.js
@@ -3,3 +3,19 @@ App.ImagesRoute = Ember.Route.extend({
     return App.imageData();
   }
 });
+
+App.ImageShowRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.modelFor("images").findBy("id", params.id);
+  },
+
+  actions: {
+    downloadImage: function() {
+      var link = document.createElement("a");
+      var uri = Ember.$("img.pixelated-image").attr("src");
+      link.download = "pixelfy.png";
+      link.href = uri;
+      link.click();
+    }
+  }
+});
