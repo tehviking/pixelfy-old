2bca5d286fca61b30ac386bc3845ca1bd661dc5e
Brandon Hays <brandon.hays@gmail.com>
2015-02-23 22:14:33 -0600

    Upgrade component to addon and include dependencies

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import("bower_components/pixelate/pixelate.js");
+ app.import("bower_components/JavaScript-MD5/js/md5.js");
+
 module.exports = app.toTree();
